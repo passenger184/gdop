93bf521ecab2b9beed52ea4eb965f0f38fd69ace
Your Name <you@example.com>
2024-12-30 18:25:42 +0300

    vercel modification

--- landing_page_mint/settings.py
+++ landing_page_mint/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xd#is9vh1cgv78znym%94%^$fr@1jurj8(uwj^k$@+y5dyw7*3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
