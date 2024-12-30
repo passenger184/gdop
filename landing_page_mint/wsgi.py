7f9a0b888fb3d95f67a317f4666e259569d54ab8
Your Name <you@example.com>
2024-12-30 15:40:54 +0300

    second commit

--- landing_page_mint/wsgi.py
+++ landing_page_mint/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'landing_page_mint.settings')
 
 application = get_wsgi_application()
+
+app = application
