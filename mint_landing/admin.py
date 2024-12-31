059164ae52e2dbd4f31b1156ceb1edeb0ad05b56
ashenafiDL <ashenafi.debella101@gmail.com>
2024-12-31 05:11:06 +0300

    added custom theme for admin pages

--- mint_landing/admin.py
+++ mint_landing/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.models import User, Group
 
-# Register your models here.
+from unfold.admin import ModelAdmin
+
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+
+def toggle_active_status(modeladmin, request, queryset):
+    for user in queryset:
+        user.is_active = not user.is_active
+        user.save()
+    modeladmin.message_user(
+        request, "Selected users' active status has been toggled.")
+
+
+toggle_active_status.short_description = "Toggle active status of selected users"
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    actions = [toggle_active_status]
+    list_display = ["username", "first_name",
+                    "last_name", "email", "date_joined", "is_active"]
+
+
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin, ModelAdmin):
+    pass
