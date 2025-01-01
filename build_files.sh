518d67cee361be2b2aa154641b0cc72e2f880726
ashenafiDL <ashenafi.debella101@gmail.com>
2025-01-01 09:29:54 +0300

    add default superuser in deployment config

--- build_files.sh
+++ build_files.sh
@@ -12,3 +12,17 @@ cp -r staticfiles/ .vercel/output/static/
 
 python3 manage.py makemigrations
 python3 manage.py migrate
+
+# Create a default superuser
+python3 manage.py shell <<EOF
+from django.contrib.auth.models import User
+username = "admin"
+password = "admin123"
+email = "admin@example.com"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f"Superuser '{username}' created successfully.")
+else:
+    print(f"Superuser '{username}' already exists.")
+EOF
