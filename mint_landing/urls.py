ff2d599d299422fc83d6168d1f600c576cb2e584
Your Name <you@example.com>
2025-01-04 15:50:25 +0300

    updating

--- mint_landing/urls.py
+++ mint_landing/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('news', views.news, name='news'),
-    path('about', views.about, name='news'),
-    path('contact', views.contact, name='news'),
+    path('about', views.about, name='about'),
+    path('request-support', views.contact, name='contact'),
     path('vision-mission-values', views.v_m_s, name='vms'),
     path('organizational-structure', views.o_s, name='o_s'),
     path('power-duties', views.p_d, name='p_d'),
