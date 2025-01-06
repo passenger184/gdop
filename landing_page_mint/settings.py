bce18e93c109a74dda3f3c706f8228dc5903c9c7
ashenafiDL <ashenafi.debella101@gmail.com>
2025-01-06 07:33:24 +0300

    set debug to false

--- landing_page_mint/settings.py
+++ landing_page_mint/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', True)
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '127.0.0.1']
 
