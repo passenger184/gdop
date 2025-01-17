9e0e79be72e4b7bc1fb0623853da544c6e32f632
ashenafiDL <ashenafi.debella101@gmail.com>
2025-01-17 09:50:40 +0300

    set debug false

--- landing_page_mint/settings.py
+++ landing_page_mint/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv('DEBUG', default=False))
+DEBUG = False
 USE_X_FORWARDED_HOST = True
 
 # ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(',')
